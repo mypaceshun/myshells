a8bf3aac48997890ee53ff17296f1ad055e2dc48
shun <shun@osstech.co.jp>
2017-04-19 16:06:54 +0900

    init-network-cent update

--- kvm/init-network-cent.sh
+++ kvm/init-network-cent.sh
@@ -26,7 +26,7 @@ hostnamectl set-hostname $hostname
 
 
 #==========================================
-# set localIP
+# set natIP
 #==========================================
 natIP=$natIP"/24"
 gateway="192.168.122.1"
@@ -41,10 +41,18 @@ nmcli c down eth0
 nmcli c up eth0
 
 #==========================================
-# set hostname
+# set bridgeIP
 #==========================================
-#==========================================
-# set hostname
-#==========================================
+natIP=$natIP"/16"
+gateway="10.0.0.1"
+
+nmcli c modify eth1 ipv4.addresses ${natIP}
+nmcli c modify eth1 ipv4.gateway $gateway
+nmcli c modify eth1 ipv4.dns $gateway
+nmcli c modify eth1 ipv4.method manual
+
+# restart
+nmcli c down eth1
+nmcli c up eth1
 
 systemctl restart network
